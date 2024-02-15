9a6e5acd3887fbe320c75c317ecbb827209b8653
fernandouteca <146396151+fernandouteca@users.noreply.github.com>
2024-02-14 20:31:14 -0600

    main- Nuevo Script (Movimiento Simple)
    Agregue el script de Movimiento Simple, el cual ya no contiene errores.

---
+++ IsisMendoza-PPV2/Assets/Scripts/MovimientoSimple.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSimple : MonoBehaviour
+{
+    public float velocidad = 5f;
+
+    private void Update()
+    {
+        // Obtener la entrada del teclado
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal, movimientoVertical, 0f);
+
+        // Mover el objeto en la dirección calculada
+        transform.Translate(movimiento * velocidad * Time.deltaTime, Space.World);
+    }
+}
+
